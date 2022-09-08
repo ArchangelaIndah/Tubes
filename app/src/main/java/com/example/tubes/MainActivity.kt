894d58b6ccafed0cc38880f1380c3e0de562c9ae
ArchangelaIndah <200710723@students.uajy.ac.id>
2022-09-08 19:29:06 +0700

    merubah masuk dan daftar

--- app/src/main/java/com/example/tubes/MainActivity.kt
+++ app/src/main/java/com/example/tubes/MainActivity.kt
@@ -1,13 +1,61 @@
 package com.example.tubes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var inputUsername: TextInputLayout
+    private lateinit var inputPassword: TextInputLayout
+    private lateinit var mainLayout: ConstraintLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("Haloo")
+        setTitle("User Login")
+
+        inputUsername = findViewById(R.id.inputLayoutUsername)
+        inputPassword = findViewById(R.id.inputLayoutPassword)
+        mainLayout = findViewById(R.id.mainLayout)
+        val btnMasuk: Button = findViewById(R.id.btnMasuk)
+        val btnDaftar: Button = findViewById(R.id.btnDaftar)
+
+        btnMasuk.setOnClickListener{
+            inputUsername.getEditText()?.setText("")
+            inputPassword.getEditText()?.setText("")
+
+            Snackbar.make(mainLayout,"Text Cleared Success", Snackbar.LENGTH_LONG).show()
+        }
+
+        btnMasuk.setOnClickListener(View.OnClickListener {
+            var checkLogin = false
+            var username: String = inputUsername.getEditText()?.getText().toString()
+            val password: String = inputPassword.getEditText()?.getText().toString()
+
+            if(username.isEmpty()){
+                inputUsername.setError("Username must be filled with text")
+                checkLogin = false
+            }
+            if(password.isEmpty()){
+                inputPassword.setError("Password must be filled with text")
+                checkLogin = false
+            }
+
+            if(username == "admin" && password == "0723")
+                checkLogin = true
+            if(!checkLogin)
+                return@OnClickListener
+        })
+
+        btnDaftar.setOnClickListener{
+            val moveHome = Intent(this, Register::class.java)
+            startActivity(moveHome)
+        }
     }
 }
